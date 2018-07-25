0c285df963c417e215a0733ce888e7a1af845b70
TylerRook <32176613+TylerRook@users.noreply.github.com>
2018-07-24 22:29:36 -0500

    First implementation of the algorithm.

---
+++ Luhn_Algorithm.py
@@ -0,0 +1,18 @@
+def checkLuhn(cardNumber):
+    cardNumbers = [int(digit) for digit in str(cardNumber)]
+    isOdd = True
+    total = 0
+    for digit in cardNumbers:
+        if isOdd:
+            digit = 2*digit
+            digit = digit//10 + digit%10
+            total += digit
+            isOdd = False
+        else:
+            total += digit
+            isOdd = True
+    valid = (total % 10 == 0)
+    if valid:
+        print("Credit Card", cardNumber, "is valid.")
+    else:
+        print("Credit Card", cardNumber, "is not valid.")
