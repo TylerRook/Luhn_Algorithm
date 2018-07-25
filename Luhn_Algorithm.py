25bdae3ce5af3ec06f7c59c7c1c2db7011885c20
TylerRook <32176613+TylerRook@users.noreply.github.com>
2018-07-25 12:08:34 -0500

    Optimized the code.

--- Luhn_Algorithm.py
+++ Luhn_Algorithm.py
@@ -1,19 +1,12 @@
 # Implements the Luhn Algorithm to validate Credit Card Numbers.
 def checkLuhn(cardNumber):
-    cardNumbers = [int(digit) for digit in str(cardNumber)]
-    isOdd = True
+    digits = [int(digit) for digit in str(cardNumber)]
     total = 0
-    for digit in cardNumbers:
-        if isOdd:
-            digit = 2*digit
-            digit = digit//10 + digit%10
-            total += digit
-            isOdd = False
-        else:
-            total += digit
-            isOdd = True
-    valid = (total % 10 == 0)
-    if valid:
+    for i in range(-1, -len(digits) - 1, -1):
+        if i % 2 == 0:
+            digits[i] = (2*digits[i])//10 + (2*digits[i])%10
+        total += digits[i]
+    if total % 10 == 0:
         print("Credit Card", cardNumber, "is valid.")
     else:
         print("Credit Card", cardNumber, "is not valid.")
